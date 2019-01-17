057622ed8396b7ede09b5c2a7d7563013bf24cd9
Jhoneagle <eaglekjohn@gmail.com>
2019-01-17 11:53:42 +0200

    update primeNumbers.cpp

--- primeNumbers.cpp
+++ primeNumbers.cpp
@@ -4,10 +4,32 @@
 using namespace std;
 
 int main() {
-  long long tests;
-  cin >> tests;
+  long long n;
+  cin >> n;
+  vector<bool> primes(n * n, true);
+  vector<bool> visits(n * n, false);
+
+  for(int i = 2; (i * 2) <= (n * n); i++) {
+    for(int j = 2; (j * i) <= (n * n); j++) {
+        primes[j * i] = false;
+    }
+  }
   
+  for(int i = 1; i <= n; i++){
+    cout << i, cout << " ";
+  }
   
+  cout << "\n";
+  
+  for(int i = 1; i <= n; i++){
+    for(int j = n; j >= 1; j--){
+      if((primes[j + i] == true) && (visits[j] == false)) {
+        cout << j, cout << " ";
+	visits[j] = true;
+	break;
+      }
+    }
+  }
   
   return 0;
 }
