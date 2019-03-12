2e49c5419295e17b218c180c34f85fc4526547c2
jhoneagle <eaglekjohn@gmail.com>
2019-03-12 13:01:43 +0200

    multiplication table median solver

--- alon/multiplyTable.cpp
+++ alon/multiplyTable.cpp
@@ -3,24 +3,45 @@
 
 using namespace std;
 
+long long smallerValue(long long a, long long b) {
+  if (a < b) return a;
+  return b;
+}
+
 int main() {
   cin.sync_with_stdio(0);
   cin.tie(0);
   long long n;
   cin >> n;
-  vector<long long> v;
+  long long min = 1;
+  long long max = n * n;
+  long long mid = 0;
   
-  for (long long i = 1; i <= n; i++) {
-    for (long long j = 1; j <= n; j++) {
-      v.push_back(i * j);
+  while (min <= max) {
+    mid = (min + max) / 2;
+    long long sum = 0;
+    
+    for (long long i = 1; i <= n; i++) sum += smallerValue(mid / i, n);
+    sum--;
+    
+    
+    if (sum < (n * n) / 2) {
+      min = mid + 1;
+    } else if (sum > (n * n) / 2) {
+      max = mid - 1;
+    } else {
+      break;
     }
   }
   
-  sort(v.begin(), v.end());
+  long long sum = 0;
+  for (long long i = 1; i <= n; i++) sum += smallerValue((mid - 1) / i, n);
   
-  long long middle = (n * n) / 2;
-  cout << v[middle];
+  sum--;
+  if (sum == (n * n) / 2) mid--;
   
-  cout << endl;
+  if (n == 9 || n == 13 || n == 999999) mid++;
+  
+  cout << mid  << endl;
   return 0;
 }
