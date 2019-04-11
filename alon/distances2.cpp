52c81ec37798735dc469560d8010d5db284aa62a
jhoneagle <eaglekjohn@gmail.com>
2019-04-11 17:06:36 +0300

    one more ready

--- alon/distances2.cpp
+++ alon/distances2.cpp
@@ -5,8 +5,35 @@ using namespace std;
 
 typedef long long number;
 
-number n, result, temp;
+number n;
 vector<number> tree[500001];
+number childrens[500001];
+number sumDistances[500001];
+
+void dfs(number node, number prev) {
+  number sum = 0;
+  number amount = 1;
+  
+  for (auto next : tree[node]) {
+    if (next == prev) continue;
+    
+    dfs(next, node);
+    sum += childrens[next] + sumDistances[next];
+    amount += childrens[next];
+  }
+  
+  sumDistances[node] = sum;
+  childrens[node] = amount;
+}
+
+void includeParent(number node, number parent) {
+  sumDistances[node] = sumDistances[parent] - childrens[node] + (n - childrens[node]);
+  
+  for (auto next : tree[node]) {
+    if (next == parent) continue;
+    includeParent(next, node);
+  }
+}
 
 int main() {
   cin.sync_with_stdio(0);
@@ -25,8 +52,10 @@ int main() {
     tree[b].push_back(a);
   }
   
+  dfs(1, 0);
+  for (auto go : tree[1]) includeParent(go, 1);
   
-  
+  for (number i = 1; i <= n; i++) cout << sumDistances[i] << " ";
   cout << endl;
   return 0;
 }
---
+++ alon/planets.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long number;
+
+number n, q;
+
+
+int main() {
+  cin.sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> q;
+  
+  
+  
+  cout << endl;
+  return 0;
+}
