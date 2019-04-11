3e69b6ddd82324311def1409b0c8eaae4e46199d
jhoneagle <eaglekjohn@gmail.com>
2019-04-11 17:30:13 +0300

    stuck again -.-

--- alon/planets.cpp
+++ alon/planets.cpp
@@ -5,15 +5,24 @@ using namespace std;
 
 typedef long long number;
 
-number n, q;
-
+number n, q, result;
 
 int main() {
   cin.sync_with_stdio(0);
   cin.tie(0);
   cin >> n >> q;
+  number jump[n + 1];
   
+  for (number i = 1; i <= n; i++) cin >> jump[i];
   
+  number a, b;
+  for (number i = 1; i <= q; i++) {
+    cin >> a >> b;
+    result = a;
+    
+    for (number i = 1; i <= b; i++) result = jump[result];
+    cout << result << "\n";
+  }
   
   cout << endl;
   return 0;
